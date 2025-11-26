1b1b6d6cea6354feba36c549082c71b684795fd2
Lucas Dantas <lucas.dantas@mobilemed.com.br>
2025-11-26 16:58:15 -0300

    Tudo certo

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:4200', 'http://127.0.0.1:4200'], // frontend Angular
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // se precisar enviar cookies
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
